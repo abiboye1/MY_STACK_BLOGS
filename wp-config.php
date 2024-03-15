0f44224d3b305ce2a193500d7ca78ecbac707db7
abiboye1 <159666903+abiboye1@users.noreply.github.com>
2024-03-15 04:03:39 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'username_here' );
 define( 'DB_PASSWORD', 'password_here' );
 
 /** Database hostname */
-define( 'DB_HOST', 'rds_instance' );
+define( 'RDS_INSTANCE', 'rds_instance' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
